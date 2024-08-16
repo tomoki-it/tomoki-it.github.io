b1881454239aed3f9346bb6bbd1429f36716f276
Tomoki-I <86475540+tomoki-it@users.noreply.github.com>
2024-08-16 22:45:54 +0900

    Add files via upload

--- main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-    var circleContainer = document.getElementById('circleContainer');
-    var circleImage = document.getElementById('circleImage');
-
-    circleContainer.style.width = circleImage.width + 'px';
-    circleContainer.style.height = circleImage.height + 'px';
-});
